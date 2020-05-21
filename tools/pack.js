0cbd4b3a0334145ad03d1c1935e96e3e64f01bd0
Tom Steele <tom.steele.94@gmail.com>
2020-05-21 16:22:03 +0200

    Made build process work cross-platform

---
+++ tools/pack.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const outFolder = path.resolve('new');
+
+if (! fs.existsSync(outFolder)) {
+    fs.mkdirSync(outFolder);
+}
+
+const files = ['Purpl.exe', 'offsets.json', 'config.json', 'taskDependents']
+
+files.forEach(file => fs.copySync(path.resolve(file), path.resolve(outFolder, file)));
